af6d54a6f0e7a7921b977ef75e650ed4301348ff
ruhulkuddus-dsi <ruhul.kuddus@dsinnovators.com>
2025-12-02 21:25:21 +0600

    first commit

---
+++ Basic/src/EvenOdd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if(n%2==0){
+            System.out.println("The number is even");
+        }else {
+            System.out.println("The number is odd");
+        }
+    }
+}
