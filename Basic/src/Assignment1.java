7742189fb983fcb4ace33ea83654ecccfb5aa3de
mdruhulkuddus <mdruhulkudduseasin@gmail.com>
2025-12-02 22:38:10 +0600

    Assignment-1: Myself and learning summary

---
+++ Basic/src/Assignment1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        // Assignment: Simple Printing Program
+
+
+        //1st block about me
+        System.out.println("Name: Md Ruhul Kuddus");
+        System.out.println("Course: Spring Boot Development");
+        System.out.println("My goal is to master RESTful API development.");
+        System.out.println("Dec 02, 2025");
+        System.out.println("Hello, fellow developers!");
+
+        // Module 1 summary
+        System.out.println("");
+        System.out.println("### Module Summary ###");
+        System.out.println("I learned from module 1: Java environment setup, Java Basic Syntax, and Project structure");
+        System.out.println("I created some project previous: like Hello world, Even Odd and previously i make some project");
+        System.out.println("I learned how to run code in java, i now stated learning, inshaaAllah, i make more useful project by java and springBoot");
+
+    }
+}
